bda82291e2ca805295db3239bd76f62ac696b5cb
mutua-muvevi <samstore637@gmail.com>
2024-05-10 22:25:47 +0300

    2d list

--- list.py
+++ list.py
@@ -19,52 +19,52 @@ countries = [
 	"western sahara", "zambia", "zimbabwe"
 ]
 
-#
-# print(iterable_list)
-#
-# # if chipo is in the array it should print not healthy
-# for i in foods:
-# 	if i == "chipo":
-# 		print("Unhealthy food detected", i)
-#
-# # change the beginning on every string to uppercase
-# for i in range(len(foods)):
-# 	foods[i] = foods[i].capitalize()
-# print(foods)
-#
-# # while loop
-# counter = 0
-#
-# print(foods)
-#
-# # slicing an index
-# # slicing list return new list it doesn't affect the original one [start:end:step]
-# sliced_list = foods[0:3]
-# print(sliced_list)
-#
-#
-# # given a beginning and last number print every number in an increment of 3
-# numbers = list(range(int(input("Enter the number here : "))))
-# end_number = int(input("Enter the last number : "))
-# plus_3 = numbers[:end_number:3]
-# print(plus_3)
-#
-# # list unpacking
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10]
-# first, second, third = numbers[0], numbers[1], numbers[2]
-#
-# # if we only want to unpack two
-# first_num, second_num, *others, last = numbers
-#
-#
-# while counter < len(foods):
-# 	foods[counter] = foods[counter].capitalize()
-# print(first_num, second_num, others, last)
-#
-#
-# for i in enumerate(numbers):
-# 	print(i)
-#
+
+print(iterable_list)
+
+# if chipo is in the array it should print not healthy
+for i in foods:
+	if i == "chipo":
+		print("Unhealthy food detected", i)
+
+# change the beginning on every string to uppercase
+for i in range(len(foods)):
+	foods[i] = foods[i].capitalize()
+print(foods)
+
+# while loop
+counter = 0
+
+print(foods)
+
+# slicing an index
+# slicing list return new list it doesn't affect the original one [start:end:step]
+sliced_list = foods[0:3]
+print(sliced_list)
+
+
+# given a beginning and last number print every number in an increment of 3
+numbers = list(range(int(input("Enter the number here : "))))
+end_number = int(input("Enter the last number : "))
+plus_3 = numbers[:end_number:3]
+print(plus_3)
+
+# list unpacking
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 10]
+first, second, third = numbers[0], numbers[1], numbers[2]
+
+# if we only want to unpack two
+first_num, second_num, *others, last = numbers
+
+
+while counter < len(foods):
+	foods[counter] = foods[counter].capitalize()
+print(first_num, second_num, others, last)
+
+
+for i in enumerate(numbers):
+	print(i)
+
 
 # loop through list of countries and add ea countries to africa_mashariki in uppercase
 africa_mashariki = []
@@ -84,5 +84,12 @@ while counter < len(countries):
 		countries[counter] = countries[counter].capitalize()
 		africa_mashariki.append(countries[counter])
 	counter += 1
-	
+
 print(africa_mashariki)
+
+# a program that prints the largest number in a list
+numbers_in_a_list = [10, 12, 3, 4, 5, 6]
+numbers_in_a_list.sort()
+
+largest_number = numbers_in_a_list[-1]
+print(largest_number)
---
+++ list_2d.py
@@ -0,0 +1,10 @@
+matrix = [
+	[1, 2, 3],
+	[4, 5, 6],
+	[7, 8, 9]
+]
+
+for row in matrix:
+	for items in row:
+		print(items, " ", end="")
+	print(row)
