ff1c5eebc8133dc8dd1b18aad3f6066f0032fccf
mutua-muvevi <samstore637@gmail.com>
2024-05-12 00:21:43 +0300

    working on functions

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import math
+
+
 # # emoji converter
 #
 # emojis = {
@@ -22,21 +25,35 @@
 # output = emoji_converter(sentence)
 # print(output)
 
-# a function that creates user object for you
-user_object = {}
+# # a function that creates user object for you
+# user_object = {}
+#
+# name = input("What is your name : ")
+# age = int(input("What is your age : "))
+# profession = input("What is your profession : ")
+#
+#
+# # creates turple when you do it this way
+# def create_user(name, age, profession):
+# 	user_object[name, profession, age] = name, profession, age
+#
+# 	return user_object
+#
+#
+# new_user = create_user(name, age, profession)
+# print((new_user))
+#
+#
+# for keys, value in new_user.items():
+# 	print("Keys", keys, type(keys))
+# 	print("Values", value)
 
-name = input("What is your name : ")
-age = int(input("What is your age : "))
-profession = input("What is your profession : ")
+# key word arguments
+def calculate_area(radius, units):
+	area = math.pi * pow(radius, 2)
+	return area, units
 
 
-def create_user(name, age, profession):
-	user_object[name] = name
-	user_object[profession] = profession
-	user_object[age] = profession
-	
-	return user_object
-
-
-new_user = create_user(name, age, profession)
-print(new_user)
+area_circle = calculate_area(units="metres", radius=int(input("Enter the radius")))
+print(area_circle)
+print(type(area_circle))
