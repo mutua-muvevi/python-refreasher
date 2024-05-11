7a8bf4f0983cc0eb9ced070197621eb3a613f5cf
mutua-muvevi <samstore637@gmail.com>
2024-05-12 00:35:22 +0300

    working on functions

--- functions.py
+++ functions.py
@@ -1,25 +1,25 @@
 import math
 
 
-# # emoji converter
-#
-# emojis = {
-# 	":)": "😊",
-# 	":(": "😔",
-# 	":D": "🤣"
-# }
-#
-# sentence = input(">> ")
-#
-#
-# def emoji_converter(input_sentence):
-# 	output = ""
-# 	words = input_sentence.split(" ")
-#
-# 	for word in words:
-# 		output += emojis.get(word, word) + " "
-#
-# 	return output
+# emoji converter
+
+emojis = {
+	":)": "😊",
+	":(": "😔",
+	":D": "🤣"
+}
+
+sentence = input(">> ")
+
+
+def emoji_converter(input_sentence):
+	output = ""
+	words = input_sentence.split(" ")
+
+	for word in words:
+		output += emojis.get(word, word) + " "
+
+	return output
 #
 #
 # output = emoji_converter(sentence)
